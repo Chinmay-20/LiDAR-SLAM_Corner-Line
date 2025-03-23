b92252622d551c73e17d5a3f9d1bef48a172fddc
Chinmay Kulkarni <59417006+Chinmay-20@users.noreply.github.com>
2025-03-23 11:37:48 -0400

    Delete deletefolder directory

--- deletefolder/helpertransformtovector.m
+++
@@ -1,9 +0,0 @@
-function v = helpertransformtovector(A)
-% T2V homogeneous transformation to vector
-% A =  cos -sin x
-%      sin cos y
-%      0    0   1
-v(1:2,1) = A(1:2,3);
-v(3,1) = atan2(A(2,1), A(1,1));
-
-end
